85489b1c950492d1fc3c7b93f11190e15f79b3e2
ashishgids-dotcom <ashishgids@gmail.com>
2026-08-16 12:21:22 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     // ---- CRITICAL SDK REQUIREMENTS ----
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         // Modern Java toolchain compatible with AGP 8.10 / compileSdk 36
